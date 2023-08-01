4122c0b248f5426412b6a8006a4f3756579ed72f
Murilo Margonar <murilomargonar@gmail.com>
2023-08-01 11:51:50 -0300

    Delete PatientsRepo.java

--- src/main/java/com/mrlmurilo/HSTechnology/repositories/PatientsRepo.java
+++
@@ -1,8 +0,0 @@
-package com.mrlmurilo.HSTechnology.repositories;
-
-import com.mrlmurilo.HSTechnology.models.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientsRepo extends JpaRepository<Patient, Long> {
-
-}
