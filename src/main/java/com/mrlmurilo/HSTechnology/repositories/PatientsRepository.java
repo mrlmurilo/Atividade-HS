a795dc41651f6f813476f93d42bece43a4caf99b
Murilo Margonar <murilomargonar@gmail.com>
2023-08-01 11:51:34 -0300

    Atualizando o Repository

--- src/main/java/com/mrlmurilo/HSTechnology/repositories/PatientsRepository.java
+++ src/main/java/com/mrlmurilo/HSTechnology/repositories/PatientsRepository.java
@@ -5,5 +5,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientsRepository extends JpaRepository<Patient, Long> {
- List<Patient> findByNameContainingIgnoreCase(String name);
+   List<Patient> findByNameContainingIgnoreCase(String name);
 }
